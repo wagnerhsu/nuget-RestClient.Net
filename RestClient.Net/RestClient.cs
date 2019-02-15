b2992a1ffb5499438aae792c6586c042cec0715d
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-02-16 08:02:46 +1100

    Code compiles

--- RestClient.Net/RestClient.cs
+++ RestClient.Net/RestClient.cs
@@ -19,7 +19,7 @@ namespace RestClientDotNet
         public Uri BaseUri => _HttpClient.BaseAddress;
         public Dictionary<string, string> Headers { get; private set; } = new Dictionary<string, string>();
         public AuthenticationHeaderValue Authorization { get; set; }
-        public Dictionary<HttpStatusCode, Func<byte[], object>> HttpStatusCodeFuncs = new Dictionary<HttpStatusCode, Func<byte[], object>>();
+        public Dictionary<HttpStatusCode, Func<byte[], object>> HttpStatusCodeFuncs { get; } = new Dictionary<HttpStatusCode, Func<byte[], object>>();
         public IZip Zip { get; set; }
         public ISerializationAdapter SerializationAdapter { get; }
         #endregion
